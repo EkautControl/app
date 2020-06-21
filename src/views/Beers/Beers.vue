a3b0ade47847bbaa06e86bd83f2d674b4ba52924
Anna Gabriela <annagmcarvalho@gmail.com>
2020-06-21 07:01:19 -0300

    adjust routes

---
+++ src/views/Beers/Beers.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <PageHeader>
+      <Button text="Nova cerveja" icon="plus.svg" size="lg" />
+    </PageHeader>
+  </div>
+</template>
+
+<script>
+import PageHeader from '../../components/PageHeader.vue';
+import Button from '../../components/Button.vue';
+
+export default {
+  components: {
+    PageHeader,
+    Button,
+  },
+};
+</script>
