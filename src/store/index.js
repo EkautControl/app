3dce449828a4c6a1d0eec2ffbe2c28e8d6dc3edd
Anna Gabriela <annagmcarvalho@gmail.com>
2020-06-21 21:24:53 -0300

    add to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import moduleSideBar from './sideBar';
+import moduleLoading from './loading';
 import moduleTanks from './tanks';
 import moduleBeers from './beers';
+import moduleActivities from './activities';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     sidebar: moduleSideBar,
+    loading: moduleLoading,
     tanks: moduleTanks,
     beers: moduleBeers,
+    activities: moduleActivities,
   },
 });
