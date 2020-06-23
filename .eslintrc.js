bb0724e11e48422f97a7d328fb8390146d36546e
Anna Gabriela <annagmcarvalho@gmail.com>
2020-06-23 06:06:35 -0300

    allow undescore

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,18 +3,15 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-    'prettier',
-  ],
-  "plugins": ['prettier'],
+  extends: ['plugin:vue/essential', '@vue/airbnb', 'prettier'],
+  plugins: ['prettier'],
   parserOptions: {
     parser: 'babel-eslint',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-underscore-dangle': 0,
     'prettier/prettier': ['error'],
   },
 };
