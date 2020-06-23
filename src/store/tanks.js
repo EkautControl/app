c2daac489e9b54b9abd449d2c6454245dc94b60a
Anna Gabriela <annagmcarvalho@gmail.com>
2020-06-23 06:38:17 -0300

    fix tank store to sort tanks on getter

--- src/store/tanks.js
+++ src/store/tanks.js
@@ -7,13 +7,17 @@ export default {
   }),
   getters: {
     getActiveTanks(state) {
-      return state.activeTanks.filter((tank) => !tank.production.hasProblem);
+      return state.activeTanks
+        .filter((tank) => !tank.production.hasProblem)
+        .sort((a, b) => a.tank - b.tank);
     },
     getTanksInAlert(state) {
-      return state.activeTanks.filter((tank) => tank.production.hasProblem);
+      return state.activeTanks
+        .filter((tank) => tank.production.hasProblem)
+        .sort((a, b) => a.tank - b.tank);
     },
     getInactiveTanks(state) {
-      return state.inactiveTanks;
+      return state.inactiveTanks.sort((a, b) => a.tank - b.tank);
     },
   },
   mutations: {
@@ -24,6 +28,16 @@ export default {
     async requestInactiveTanks(state) {
       const tanks = await axios.get('/inactiveTanks');
       state.inactiveTanks = tanks.data;
+    },
+    async addProductionToTank(state, payload) {
+      const newProd = await axios.post('/production', {
+        tank: payload.tank,
+        beerId: payload.beerId,
+        batch: payload.batch,
+        phase: payload.phase,
+        date: payload.date,
+      });
+      state.activeTanks.push(newProd.data);
     },
   },
 };
