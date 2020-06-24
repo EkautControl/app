734e06a371f6ec3277bafd003675f5ce55570c76
Anna Gabriela <annagmcarvalho@gmail.com>
2020-06-23 22:32:53 -0300

    automate heroku build

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const serveStatic = require('serve-static');
 const { join } = require('path');
 
 const app = express();
+app.use(require('connect-history-api-fallback')());
+
 app.use(serveStatic(join(__dirname, 'dist')));
 const port = process.env.PORT || 80;
 app.listen(port);
