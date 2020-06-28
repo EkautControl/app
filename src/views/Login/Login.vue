2c1d557d8185c9b77bc2e4639f38ca7c7e25ec7b
Anna Gabriela <annagmcarvalho@gmail.com>
2020-06-28 13:38:51 -0300

    add login animation

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,23 +1,24 @@
 <template>
-  <div class="login-screen">
+  <div :class="`login-screen  ${animation ? 'beer-animation' : ''}`">
     <section class="login-card">
       <div class="logos">
-        <img class="logo ekaut" src="@/assets/ekaut-logo.svg"
-          alt="Ekaut Logo"/>
-        <img class="logo control" src="@/assets/logo.svg"
-          alt="Ekaut Control logo"/>
+        <img class="logo ekaut" src="@/assets/ekaut-logo.svg" alt="Ekaut Logo" />
+        <img class="logo control" src="@/assets/logo.svg" alt="Ekaut Control logo" />
       </div>
       <b-field
         class="sign-in-form fields"
         :type="alertInvalid"
         label="Email"
-        label-position="inside">
-        <b-input type="email"
+        label-position="inside"
+      >
+        <b-input
+          type="email"
           custom-class="sign-in-input"
           v-model="email"
           @focus="handleFocus"
           @blur="handleBlur"
-          @keyup.enter.native="handleLogin">
+          @keyup.enter.native="handleLogin"
+        >
         </b-input>
       </b-field>
       <b-field
@@ -25,14 +26,17 @@
         :type="alertInvalid"
         :message="invalid"
         label="Senha"
-        label-position="inside">
-        <b-input type="password"
+        label-position="inside"
+      >
+        <b-input
+          type="password"
           v-model="password"
           custom-class="sign-in-input"
           @focus="handleFocus"
           @blur="handleBlur"
           @keyup.enter.native="handleLogin"
-          password-reveal>
+          password-reveal
+        >
         </b-input>
       </b-field>
       <b-button
@@ -40,13 +44,12 @@
         class="sign-in-form button"
         @click="handleLogin"
         type="is-link"
-        rounded>Entrar</b-button>
-      <b-button
-        v-if="signingIn"
-        class="sign-in-form"
-        type="is-link"
         rounded
-        loading>Loading</b-button>
+        >Entrar</b-button
+      >
+      <b-button v-if="signingIn" class="sign-in-form" type="is-link" rounded loading
+        >Loading</b-button
+      >
     </section>
   </div>
 </template>
@@ -64,24 +67,23 @@ export default {
       signingIn: false,
       invalid: '',
       alertInvalid: '',
+      animation: false,
     };
   },
-
-  components: {
-  },
-
   methods: {
     async handleLogin(event) {
       event.preventDefault();
       this.signingIn = true;
 
       try {
+        this.animation = true;
         await Auth.signIn(this.email, this.password);
         this.signingIn = false;
         this.$store.dispatch('getUser', this.email);
         this.$router.push({ name: 'Main' });
       } catch (e) {
         this.signingIn = false;
+        this.animation = false;
         this.alertInvalid = 'is-danger';
         this.invalid = 'Email ou senha inválidos';
       }
@@ -116,14 +118,14 @@ export default {
 }
 
 .sign-in-form.button {
-  background-color: #3396E1;
+  background-color: #3396e1;
   border-radius: 12px;
   margin-top: 40px;
   margin-bottom: 40px;
 }
 
 .sign-in-form.button:hover {
-  background-color: #3396E1;
+  background-color: #3396e1;
 }
 
 .sign-in-input {
@@ -162,9 +164,9 @@ export default {
 
 @media screen and (max-width: 768px) {
   .sign-in-form {
-  min-width: 150px;
-  width: 20%;
-  margin-bottom: 10px;
+    min-width: 150px;
+    width: 20%;
+    margin-bottom: 10px;
   }
 
   .login-screen {
@@ -200,21 +202,20 @@ export default {
   }
 }
 
-@media screen and (max-width: 1440px) {
+@media screen and (max-width: 4000px) {
   .login-card {
-  display: flex;
-  min-height: 340px;
-  min-width: 300px;
-  height: 70vh;
-  width: 35%;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  margin: auto;
-  background-color: #345087;
-  border-radius: 26px;
-  padding-top: 10px;
-  padding-bottom: 10px;
+    display: flex;
+    min-height: 340px;
+    min-width: 300px;
+    width: 35%;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    margin: auto;
+    background-color: #345087;
+    border-radius: 26px;
+    padding-top: 10px;
+    padding-bottom: 10px;
   }
 
   .ekaut {
@@ -233,5 +234,34 @@ export default {
     width: 30%;
     margin-bottom: 10px;
   }
+}
+
+.beer-animation {
+  background-image: url('../../assets/beer.svg');
+  background-size: cover;
+  background-repeat: repeat-x;
+  background-position: 0 0;
+  animation: animate 5s linear infinite;
+}
+
+@keyframes animate {
+  0% {
+    background-position: 0 700px;
+  }
+  25% {
+    background-position: 1000px 400px;
+  }
+  50% {
+    background-position: 2000px 100px;
+  }
+  75% {
+    background-position: 3000px 0;
+  }
+  100% {
+    background-position: 4000px -100px;
+  }
+}
+input:-internal-autofill-selected {
+  background-color: transparent !important;
 }
 </style>
