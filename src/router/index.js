79d7c47de067fa1482521d61221dc3da6805fed9
Anna Gabriela Carvalho <agmc@cesar.school>
2021-09-22 12:33:06 -0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import { Auth } from 'aws-amplify';
 import routes from './routes';
 
 Vue.use(VueRouter);
@@ -8,20 +7,6 @@ Vue.use(VueRouter);
 const router = new VueRouter({
   mode: 'history',
   routes,
-});
-
-router.beforeResolve(async (to, from, next) => {
-  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
-  if (requiresAuth) {
-    try {
-      await Auth.currentAuthenticatedUser();
-      next();
-    } catch (e) {
-      next({ name: 'Login' });
-    }
-  } else {
-    next();
-  }
 });
 
 export default router;
